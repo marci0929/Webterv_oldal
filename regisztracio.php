722536116759a98382e6276ad9ffb4364a70f47e
Riczz <csinosricsi@gmail.com>
2021-04-16 12:20:03 +0200

    Átirányítás Sikeres regisztrációkor átirányítás

--- regisztracio.php
+++ regisztracio.php
@@ -1,13 +1,13 @@
 <?php
-    session_start();
-    $currentPage = 'Belepes';
+session_start();
+$currentPage = 'Belepes';
 
-    require_once('php/RegistrationChecker.php');
+require_once('php/RegistrationChecker.php');
 
-    if (isset($_POST['regisztralas'])) {
+if (isset($_POST['regisztralas'])) {
     $registerValidator = new RegistrationChecker($_POST);
     $errors = $registerValidator->validate();
-    }
+}
 
 ?>
 
@@ -35,53 +35,64 @@
     <div style="margin-top: 2em;">
         <section>
             <h1 style="text-align: center">Itt tudsz regisztrálni egy új fiókot!</h1>
-                <form style="text-align: left;margin-left: 40%" method="POST" action="<?php echo $_SERVER['PHP_SELF'] ?>">
-                	<div class="input-group">
-                	  <label>Felhasználónév (kis és nagybetűket, valamit számokat tartalmazhat)</label><br>
+            <form style="text-align: left;margin-left: 40%" method="POST" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+                <div class="input-group">
+                    <label>Felhasználónév (kis és nagybetűket, valamit számokat tartalmazhat)</label><br>
 
-                	  <input type="text" name="username" value="<?php if (isset($_POST['username'])) echo $_POST['username']; ?>">
+                    <input type="text" name="username"
+                           value="<?php if (isset($_POST['username'])) echo $_POST['username']; ?>">
 
-                   <p class="error"><?php echo isset($errors['username']) ? $errors['username'] : '' ?></p>
+                    <p class="error"><?php echo isset($errors['username']) ? $errors['username'] : '' ?></p>
 
-                	</div>
-                	<div class="input-group">
-                	  <label>Email cím</label><br>
+                </div>
+                <div class="input-group">
+                    <label>Email cím</label><br>
 
-                	  <input type="email" name="email" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>">
-                  <p class="error"><?php echo isset($errors['email']) ? $errors['email'] : '' ?></p>
-                	</div>
-                  <div class="input-group">
+                    <input type="email" name="email" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>">
+                    <p class="error"><?php echo isset($errors['email']) ? $errors['email'] : '' ?></p>
+                </div>
+                <div class="input-group">
                     <label>Életkorod (ha nem múltál el 16 éves, nem regisztrálhatsz)</label><br>
 
-                    <input type="number" name="eletkor" value="<?php if (isset($_POST['eletkor'])) echo $_POST['eletkor']; ?>">
-                     <p class="error"><?php echo isset($errors['eletkor']) ? $errors['eletkor'] : '' ?></p>
-                  </div>
-                	<div class="input-group">
-                	  <label>Jelszó</label><br>
+                    <input type="number" name="eletkor"
+                           value="<?php if (isset($_POST['eletkor'])) echo $_POST['eletkor']; ?>">
+                    <p class="error"><?php echo isset($errors['eletkor']) ? $errors['eletkor'] : '' ?></p>
+                </div>
+                <div class="input-group">
+                    <label>Jelszó</label><br>
 
-                	  <input type="password" name="password1" value="<?php if (isset($_POST['password1'])) echo $_POST['password1']; ?>">
-                      <p class="error"><?php echo isset($errors['password1']) ? $errors['password1'] : '' ?></p>
-                	</div>
-                	<div class="input-group">
-                	  <label>Ismételd meg a jelszót</label><br>
+                    <input type="password" name="password1"
+                           value="<?php if (isset($_POST['password1'])) echo $_POST['password1']; ?>">
+                    <p class="error"><?php echo isset($errors['password1']) ? $errors['password1'] : '' ?></p>
+                </div>
+                <div class="input-group">
+                    <label>Ismételd meg a jelszót</label><br>
 
-                	  <input type="password" name="password2" value="<?php if (isset($_POST['password2'])) echo $_POST['password2']; ?>">
-                      <p class="error"><?php echo isset($errors['password2']) ? $errors['password2'] : '' ?></p>
-                	</div>
-                  <div class="input-group">
+                    <input type="password" name="password2"
+                           value="<?php if (isset($_POST['password2'])) echo $_POST['password2']; ?>">
+                    <p class="error"><?php echo isset($errors['password2']) ? $errors['password2'] : '' ?></p>
+                </div>
+                <div class="input-group">
                     <label>Feltölthetsz egy profilképet is</label><br>
-                 
-                    <input type="file" name="profilkep" value="<?php if (isset($_POST['profilkep'])) echo $_POST['profilkep']; ?>">
-                      <p class="error"><?php echo isset($errors['profilkep']) ? $errors['profilkep'] : '' ?></p>
-                  </div>
-                	<div class="input-group">
-                	  <button type="submit" class="btn" name="regisztralas">Regisztráció</button>
-                	</div>
-                	<p>
-                    <p></p>
-                		Van már fiókod? <a href="belepes.php"><strong>Bejelentkezés</strong></a>
-                	</p>
-                </form>
+
+                    <input type="file" name="profilkep"
+                           value="<?php if (isset($_POST['profilkep'])) echo $_POST['profilkep']; ?>">
+                    <p class="error"><?php echo isset($errors['profilkep']) ? $errors['profilkep'] : '' ?></p>
+                </div>
+                <div class="input-group">
+                    <button type="submit" class="btn" name="regisztralas">Regisztráció</button>
+                    <span class="success"><?php if
+                        (isset($errors) && empty($errors)) {
+                            echo 'Sikeres regisztráció!';
+                            header("Location: belepes.php");
+                        }
+                        ?></span>
+                </div>
+                <p>
+                <p></p>
+                Van már fiókod? <a href="belepes.php"><strong>Bejelentkezés</strong></a>
+                </p>
+            </form>
         </section>
         <?php include_once("php/include/footer.php"); ?>
     </div>
